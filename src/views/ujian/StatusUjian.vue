b57c8a63b725e4a810af2dc43861fae042f4cf2a
shellrean <reanyusufali@gmail.com>
2020-10-21 14:07:22 +0700

    change ui for status ujian

--- src/views/ujian/StatusUjian.vue
+++ src/views/ujian/StatusUjian.vue
@@ -1,60 +1,56 @@
 <template>
 	<div class="row">
 		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6 col-lg-4">
-					<div class="card  mb-3" v-if="jadwals.length > 0">
-					  <div class="card-header">TOKEN UJIAN 
-					  	<span class="badge badge-light">{{ token.status == 0 ? 'Tidak aktif' :'Aktif' }}</span></div>
-					  <div class="card-body text-dark ">
-					    <h5 class="card-title">{{ token.token }}</h5>
-					    <p class="card-text">Token ini bersifat rahasia, berlaku untuk 15 menit.</p>
-					  </div>
-					  <div class="card-footer">
-					  	<button class="btn btn-sm btn-primary" v-if="token.status == 0" @click="rilis" :disabled="isLoading">Relese token</button>
-					  </div>
+			<div class="card">
+				<div class="card-header">
+					Ujian yang dilaksanakan
+				</div>
+				<div class="card-body">
+					<div class="mb-2">
+						<span class="badge badge-light">{{ token.status == 0 ? 'Tidak aktif' :'Aktif' }}</span>
+						<h1>{{ token.token }}</h1>
+						<button class="btn btn-sm btn-primary" v-if="token.status == 0" @click="rilis" :disabled="isLoading">Relese token</button>
 					</div>
-					<div class="alert alert-warning" v-else>
-						<strong>Informasi:</strong><br>
-						 Tidak ada ujian yang diaktifkan, aktifkan ujian pada menu jadwal ujian
+					<div class="table-responsive">
+						<table class="table table-sm table-bordered table-hover table-striped">
+							<thead>
+								<tr>
+									<th>Nama ujian</th>
+									<th>Tanggal</th>
+									<th>Mulai</th>
+									<th>Sesi</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr v-for="jadwal in jadwals" :key="jadwal.id">
+									<td>{{ jadwal.alias }}
+									<td>{{ jadwal.tanggal }}</td>
+									<td>{{ jadwal.mulai }}</td>
+									<td>
+										<div class="input-group input-group-sm">
+											<select class="custom-select" v-model="jadwal.sesi">
+												<option value="1">1</option>
+												<option value="2">2</option>
+												<option value="3">3</option>
+												<option value="4">4</option>
+											</select>
+											<div class="input-group-append">
+												<button class="btn btn-info" type="button" @click="sesiChange(jadwal.id)" :disabled="isLoading">
+													Simpan
+												</button>
+											</div>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
 					</div>
+				</div>
+				<div class="card-footer">
+					Silakan ganti sesi untuk mengubah sesi tekan Simpan
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6 col-lg-4" v-for="jadwal in jadwals">
-			<div class="card mb-3">
-			  <div class="card-header" v-text="jadwal.alias"></div>
-			  <div class="card-body">
-			  	<table class="table table-bordered">
-			  		<tr>
-			  			<td>Tanggal</td>
-			  			<td>{{ jadwal.tanggal }}</td>
-			  		</tr>
-			  		<tr>
-			  			<td>Mulai</td>
-			  			<td>{{ jadwal.mulai }}</td>
-			  		</tr>
-			  	</table>
-			  	<div class="form-group">
-					<label>Sesi</label>
-			  		
-					<div class="input-group">
-					  <select class="custom-select" v-model="jadwal.sesi">
-					    <option value="1">1</option>
-					    <option value="2">2</option>
-					    <option value="3">3</option>
-					  </select>
-					  <div class="input-group-append">
-					    <button class="btn btn-secondary" type="button" @click="sesiChange(jadwal.id)" :disabled="isLoading">
-					    	Simpan
-					    </button>
-					  </div>
-					</div>
-			  	</div>
-			  </div>
-			</div>
-		</div>
-
 	</div>
 </template>
 <script>
