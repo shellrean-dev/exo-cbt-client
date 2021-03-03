6c82e28aacf7dae40cae348eb3b0b6cfcc2dbdd8
unknown <reanyusufali@gmail.com>
2021-03-03 12:29:18 +0700

    :bug: fix: nullsafe in jadwal disable refresh

--- src/views/ujian/PesertaUjian.vue
+++ src/views/ujian/PesertaUjian.vue
@@ -12,7 +12,7 @@
                             <div class="small text-muted">Reset dan force close ujian peserta</div>
                         </div>
                         <div class="d-md-block col-sm-7">
-                            <b-button variant="primary" class="float-right" size="sm" @click="refresh" :disabled="isLoading" >
+                            <b-button variant="primary" class="float-right" size="sm" @click="refresh" :disabled="isLoading" v-if="jadwal != 0 && jadwal != ''">
                                 <i class="flaticon-refresh"></i> Refresh data
                             </b-button>
                         </div>
@@ -56,15 +56,30 @@
                     </div>
                     <div class="table-responsive-md">
                         <b-table striped hover bordered small :fields="fields" :items="filteredList" show-empty>
+                            <template v-slot:cell(show_details)="row">
+                                <b-button size="sm" @click="row.toggleDetails" :variant="row.detailsShowing ? 'danger' : 'info'"><i :class="row.detailsShowing ? 'flaticon-circle' : 'flaticon2-add'" /></b-button>
+                            </template>
+                            <template v-slot:row-details="row">
+                                <b-card>
+                                    <b-input-group size="sm">
+                                        <b-form-input
+                                        placeholder="Waktu dalam menit"
+                                        v-model="row.item.addTime"
+                                        ></b-form-input>
+                                        <b-input-group-append>
+                                        <b-button @click="addMoreTime(row.item.addTime, row.item.peserta.id); row.item.addTime = ''" :disabled="isLoading">Tambah</b-button>
+                                        </b-input-group-append>
+                                    </b-input-group>
+                                </b-card>
+                            </template>
                             <template v-slot:cell(status)="row">
                                 {{ row.item.status }}
                                 <b-button variant="danger" :disabled="isLoading" class="mr-1" size="sm" @click="forceClose(row.item.peserta_id)" 
-                                v-if="row.item.status_ujian != 1 && row.item.status_ujian != 2">
+                                v-if="row.item.status_ujian == 3">
                                     Force close
                                 </b-button>
-                                <b-button variant="outline-danger" :disabled="isLoading" size="sm" @click="resetPeserta(row.item.peserta_id)" 
-                                v-if="row.item.status_ujian != 1 && row.item.status_ujian != 2">
-                                    Reset peserta
+                                <b-button variant="outline-danger" :disabled="isLoading" size="sm" @click="resetPeserta(row.item.peserta_id)">
+                                    Reset ujian
                                 </b-button>
                             </template>
                         </b-table>
@@ -82,6 +97,7 @@ export default {
     data() {
         return {
             fields: [
+                { key: 'show_details', label: 'Detail' },
                 { key: 'peserta.no_ujian', label: 'No ujian', sortable: true },
                 { key: 'peserta.nama', label: 'Nama peserta', sortable: true },
                 { key: 'mulai_ujian', label: 'Mulai ujian', sortable: true },
@@ -106,7 +122,7 @@ export default {
         }
     },
     methods: {
-        ...mapActions('ujian', ['getPesertas', 'resetUjianPeserta', 'selesaiUjianPeserta', 'getUjianActive']),
+        ...mapActions('ujian', ['getPesertas', 'resetUjianPeserta', 'selesaiUjianPeserta', 'getUjianActive','addMoreTimeUjianSiswa']),
         async resetPeserta(id) {
             if(this.jadwal == 0) {
                 this.$swal({
@@ -116,13 +132,25 @@ export default {
                 })
                 return
             }
-            try {
-                await this.resetUjianPeserta({id: id, jadwal: this.jadwal})
-                this.getPesertas(this.jadwal)
-                this.$bvToast.toast('Ujian peserta berhasil direset', successToas())
-            } catch (error) {
-                this.$bvToast.toast(error.message, errorToas())
-            }
+            this.$swal({
+                title: 'Informasi',
+                text: "Tindakan ini akan menghapus seluruh data ujian siswa, termasuk nilai siswa jika telah di selesaikan sebelumnya.",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#c3c3c3',
+                confirmButtonText: 'Iya, Lanjutkan!'
+            }).then(async (result) => {
+                if (result.value) {
+                    try {
+                        await this.resetUjianPeserta({id: id, jadwal: this.jadwal})
+                        this.getPesertas(this.jadwal)
+                        this.$bvToast.toast('Ujian peserta berhasil direset', successToas())
+                    } catch (error) {
+                        this.$bvToast.toast(error.message, errorToas())
+                    }
+                }
+            })
         },
         async forceClose(id) {
             if(this.jadwal == 0) {
@@ -133,13 +161,49 @@ export default {
                 })
                 return
             }
-            try {
-                await this.selesaiUjianPeserta({id: id, jadwal: this.jadwal })
-                this.getPesertas(this.jadwal)
-                this.$bvToast.toast('Ujian peserta berhasil ditutup paksa', successToas())
-            } catch (error) {
-                this.$bvToast.toast(error.message, errorToas())
-            }
+            this.$swal({
+                title: 'Informasi',
+                text: "Tindakan ini akan menyelesaikan ujian siswa pada jadwal ini, dan mengenerate nilai.",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#c3c3c3',
+                confirmButtonText: 'Iya, Lanjutkan!'
+            }).then(async (result) => {
+                if (result.value) {
+                    try {
+                        await this.selesaiUjianPeserta({id: id, jadwal: this.jadwal })
+                        this.getPesertas(this.jadwal)
+                        this.$bvToast.toast('Ujian peserta berhasil ditutup paksa', successToas())
+                    } catch (error) {
+                        this.$bvToast.toast(error.message, errorToas())
+                    }
+                }
+            })
+        },
+        addMoreTime(minutes, peserta_id) {
+            this.$swal({
+                title: 'Informasi',
+                text: "Tindakan ini akan menambah waktu pengerjaan siswa, pastikan waktu yang ditambahkan tidak melebihi waktu ujian.",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#c3c3c3',
+                confirmButtonText: 'Iya, Lanjutkan!'
+            }).then(async (result) => {
+                if (result.value) {
+                    try {
+                        const network = await this.addMoreTimeUjianSiswa({
+                            minutes: parseInt(minutes),
+                            peserta_id: peserta_id,
+                            jadwal_id: this.jadwal
+                        })
+                        this.$bvToast.toast(network.message, successToas())
+                    } catch (error) {
+                        this.$bvToast.toast(error.message, errorToas())
+                    }
+                }
+            })
         },
         refresh() {
             if(this.jadwal == 0) {
