6d087713e8fabe90fbae559a404697c0b1f82588
unknown <reanyusufali@gmail.com>
2021-03-03 12:20:32 +0700

    :sparkles: add: dynamic copyright

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,19 @@
 <template>
     <footer class="c-footer">
-        <div><a href="">Extraordinary-CBT</a> {{ version }} &copy; 2020 shellrean.</div>
+        <div><a href="">Extraordinary-CBT</a> &copy; {{ year }} shellrean.</div>
      </footer>
 </template>
 <script>
     export default {
         data() {
             return {
-                version: process.env.VUE_APP_VERSION
+                version: process.env.VUE_APP_VERSION,
+                year: ''
             }
+        },
+        created() {
+            let d = new Date()
+            this.year = d.getFullYear()
         }
     }
 </script>
