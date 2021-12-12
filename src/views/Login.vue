f6796909e7faecf5665770c77d58ff63a0101211
shellrean <reanyusufali@gmail.com>
2021-12-12 21:05:57 +0700

    add: show password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,11 +4,11 @@
       <div class="container bg-white">
         <div class="py-5 px-4">
           <div class="row">
-            <div class="col-md-6 col-lg-4 bg-white h-100">
+            <div class="col-md-12 col-lg-4 mx-auto bg-white h-100">
               <div class="row h-100 justify-content-center align-items-center">
                 <div>
                   <div class="d-flex align-items-center">
-                    <div style="background-color: #373752" class="py-2 px-2 rounded mr-3 align-self-start">
+                    <div style="background-color: #fff" class="py-2 px-2 rounded mr-3 align-self-start border">
                       <img :src="set_public.logo ? set_public.logo : '/img/logo.png'" style="max-height: 46px"/>
                     </div>
                     <div >
@@ -28,16 +28,22 @@
                       <input class="form-control" :class="{ 'is-invalid' : errors.email }" type="email" placeholder="Email" v-model="data.email" required>
                       <div class="invalid-feedback" v-if="errors.email">{{ errors.email[0] }}</div>
                     </div>
-                    <div class="input-group mb-4">
+                    <div class="input-group mb-1">
                       <div class="input-group-prepend ">
                         <span class="input-group-text ">
                           <i class="flaticon-lock"></i>
                         </span>
                       </div>
-                      <input class="form-control" :class="{ 'is-invalid' : errors.password }" type="password" placeholder="Password" v-model="data.password" required>
+                      <input class="form-control" :class="{ 'is-invalid' : errors.password }" :type="passwordType" placeholder="Password" v-model="data.password" required>
                       <div class="invalid-feedback" v-if="errors.password">{{ errors.password[0] }} </div>
                     </div>
-                    <b-button :variant="isLoading ? 'secondary' : 'dark'" class="mr-1" :disabled="isLoading" type="submit">
+                    <div class="input-group mb-4">
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="showPassword" v-model="showPassword">
+                        <label class="form-check-label" for="showPassword">Show password</label>
+                      </div>
+                    </div>
+                    <b-button :variant="isLoading ? 'secondary' : 'dark'" class="mr-1" block :disabled="isLoading" type="submit">
                       {{ isLoading ? 'Processing...' : 'Login' }}
                     </b-button>
                     <a :href="baseURL+'/api/v1/login/sso'" class="btn btn-outline-dark" v-if="airlock">
@@ -74,6 +80,8 @@ export default {
         email: '',
         password: ''
       },
+      showPassword: false,
+      passwordType: 'password',
       year: '',
       airlock: false,
       version: process.env.VUE_APP_VERSION
@@ -126,6 +134,13 @@ export default {
       let name = this.auth.map(item => item.name);
       if(name.includes('airlock')) {
         this.airlock = true
+      }
+    },
+    showPassword(val) {
+      if(val) {
+        this.passwordType = 'text'
+      } else {
+        this.passwordType = 'password'
       }
     }
   }
